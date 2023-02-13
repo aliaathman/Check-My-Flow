0c35e45d9e7d822d9552b04d24e647475e91f71a
Alia Athman <aliaathmann@gmail.com>
2023-02-13 17:50:41 -0500

    Adds arrow graphic, adds ButtonController, BeatScroller, and NoteObject scripts so that we have notes fall down on game screen and disappear when correct key is pressed

--- Assets/scripts/ButtonController.cs
+++ Assets/scripts/ButtonController.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ButtonController : MonoBehaviour
 {
+    private SpriteRenderer theSr;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode keyToPress;
+
     // Start is called before the first frame update
     void Start()
     {
+        theSr = GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(keyToPress)){
+            theSr.sprite = pressedImage;
+        }
+        if(Input.GetKeyUp(keyToPress)){
+            theSr.sprite = defaultImage;
+        }
     }
 }
